c625c0b278b1c4573d3f02e49302e17f774b640a
lfaustinosITS <132519618+lfaustinosITS@users.noreply.github.com>
2023-08-07 11:58:50 -0600

    Add files via upload

---
+++ JSQ-07-minimumWithMax.js
@@ -0,0 +1,9 @@
+function minimunWithMax(array) {
+    array.forEach((element, index, array) => {
+        array[index] = element * (-1);//Multiply all numbers by -1 to make the maximum minimum
+    });
+    return Math.max(...array) * (-1)//Take the maximun of the inverted array and multiply it by -1
+}
+
+array = [-2, 3, 4, 5, -7, 2, 1];
+console.log(minimunWithMax(array));
