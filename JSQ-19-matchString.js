63ecb131ae648ac49e8b0effb03727d9e1508a38
lfaustinosITS <132519618+lfaustinosITS@users.noreply.github.com>
2023-08-17 16:09:13 -0600

    Improved code
    The function returns the part that matches the pattern. If there are no matches, it returns null.

--- JSQ-19-matchString.js
+++ JSQ-19-matchString.js
@@ -1,30 +1,46 @@
 function matchPattern(pattern, text) {
     let patternIndex = 0;
     let textIndex = 0;
+    let start = -1;
 
     while (textIndex < text.length) {
         //Search for pattern matches within the text
         if (patternIndex < pattern.length && (pattern[patternIndex] === text[textIndex] || pattern[patternIndex] === '*')) {
+            if (start === -1) {
+                if (patternIndex === 0) {
+                    start = textIndex;
+                }
+                start = textIndex - patternIndex; //Adjust index when there are '*' at the beginning
+            }
             patternIndex++;
             textIndex++;
+
 
         } else if (patternIndex === pattern.length) {//Stop when complete pattern is found
             break
         } else if (textIndex === text.length - 1) {//Stop when the text is finished without finding the pattern
-            return false;
+            return null;
         } else {
             textIndex++;
-            if (pattern[patternIndex - 1] !== '*')//Restart the search if the pattern is not complete
+            if (pattern[patternIndex - 1] !== '*') {//Restart the search if the pattern is not complete
                 patternIndex = 0;
+                start = -1
+            }
         }
+    }
+    if (patternIndex === pattern.length) {
+        const end = start + pattern.length - 1;
+        const matchedPart = text.substring(start, end + 1); // Extract the matched part of the string
+        return matchedPart;
+    } else {
+        return null;
     }
 
 
-    return patternIndex === pattern.length;
 }
 
 // Example:
-console.log(matchPattern("a*c", "zykabcd")); // Output: true
-console.log(matchPattern("a*b", "zxyabcd")); // Output: false
-console.log(matchPattern("*a*", "zcvcabcd")); // Output: true
-console.log(matchPattern("a****", "zxcabcd"));  // Output: false
+console.log(matchPattern("a*c", "zykabcd")); // Output: abc
+console.log(matchPattern("a*b", "zxyabcd")); // Output: null
+console.log(matchPattern("**a***", "zcvcabcd")); // Output: vcabcd
+console.log(matchPattern("a****", "zxcabcd"));  // Output: null
