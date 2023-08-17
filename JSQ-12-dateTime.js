efcc5a354e29fb1b54199e51e989ab8c5a287ab4
lfaustinosITS <132519618+lfaustinosITS@users.noreply.github.com>
2023-08-17 15:13:47 -0600

    New code
    The global function 'addDaysToDate' was created to modify the Date instance.

--- JSQ-12-dateTime.js
+++ JSQ-12-dateTime.js
@@ -1,14 +1,14 @@
 
-Date.prototype.addDays = function (days) {
-    const newDate = new Date(this);
+function addDaysToDate(date, days) {
+    const newDate = new Date(date);
     newDate.setDate(newDate.getDate() + days);
     return newDate;
-};
+}
 
 // Example:
 const today = new Date();
-const tomorrow = today.addDays(1); // Add one day to the current date
-const yesterday = today.addDays(-1); // Subtract one day from the current date
+const tomorrow = addDaysToDate(today, 1); // Add one day to the current date
+const yesterday = addDaysToDate(today, -1); // Subtract one day from the current date
 
 console.log(today); // Output: Current date and time
 console.log(tomorrow); // Output: Date and time of tomorrow
