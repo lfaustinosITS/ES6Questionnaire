bbb63f531f99d5ae70a720dd64dd9d5e4b66bc5b
lfaustinosITS <132519618+lfaustinosITS@users.noreply.github.com>
2023-08-17 15:01:50 -0600

    New code
    The 'maxArray' function was added, which obtains the maximum value from an array of numbers.

--- JSQ-07-minimumWithMax.js
+++ JSQ-07-minimumWithMax.js
@@ -1,9 +1,17 @@
-function minimunWithMax(array) {
-    array.forEach((element, index, array) => {
-        array[index] = element * (-1);//Multiply all numbers by -1 to make the maximum minimum
-    });
-    return Math.max(...array) * (-1)//Take the maximun of the inverted array and multiply it by -1
+function maxArray(array){
+    return Math.max(...array);
 }
 
 array = [-2, 3, 4, 5, -7, 2, 1];
-console.log(minimunWithMax(array));
+console.log(maxArray(array));
+
+
+// function minimunWithMax(array) {
+//     array.forEach((element, index, array) => {
+//         array[index] = element * (-1);//Multiply all numbers by -1 to make the maximum minimum
+//     });
+//     return Math.max(...array) * (-1)//Take the maximun of the inverted array and multiply it by -1
+// }
+
+// array = [-2, 3, 4, 5, -7, 2, 1];
+// console.log(minimunWithMax(array));
