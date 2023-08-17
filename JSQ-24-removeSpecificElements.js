d254808dae10834a80118b401edba68d034d5894
lfaustinosITS <132519618+lfaustinosITS@users.noreply.github.com>
2023-08-17 16:10:48 -0600

    Improved code
    The function removes the desired elements from the original array.

--- JSQ-24-removeSpecificElements.js
+++ JSQ-24-removeSpecificElements.js
@@ -1,12 +1,15 @@
 function removeSpecificElements(arr, elementsToRemove) {
-    const filteredArray = arr.filter((element) => !elementsToRemove.includes(element));//This filter leaves the elements that are not in elementsToRemove
-
-    return filteredArray;
+    for (const element of elementsToRemove) {
+        const index = arr.indexOf(element);
+        if (index !== -1) {
+            arr.splice(index, 1); // Remove the element from the array
+        }
+    }
 }
 
 const originalArray = [1, 2, 3, 4, 5];
 const elementsToRemove = [2, 4];
 
-const newArray = removeSpecificElements(originalArray, elementsToRemove);
+removeSpecificElements(originalArray, elementsToRemove);
 
-console.log(newArray); // Output: [1, 3, 5]
+console.log(originalArray); // Output: [1, 3, 5]
