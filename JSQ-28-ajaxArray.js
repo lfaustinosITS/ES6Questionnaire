30e4cb89afd00242817dc93fa6654f090532a17e
lfaustinosITS <132519618+lfaustinosITS@users.noreply.github.com>
2023-08-18 10:42:56 -0600

    Improved code
    Simplified code. Calls to the functions were corrected.

--- JSQ-28-ajaxArray.js
+++ JSQ-28-ajaxArray.js
@@ -1,34 +1,15 @@
 var async = {
     getAll: function (urlArray, callback) {
-        // Create an array to store the promises returned by each AJAX call
-        const promises = urlArray.map(function (url, index) {
-            return fetch(url)
-                .then(function (response) {
-                    if (!response.ok) {
-                        throw new Error("Network response was not ok");
-                    }
-                    return response.json();
-                })
-                .then(function (data) {
-                    return { [index + 1]: data }; // Resolve the promise with the AJAX result and the corresponding index
-                })
-                .catch(function (error) {
-                    console.error("Error occurred during AJAX call:", error);
-                });
+        const promises = urlArray.map((axCall, index) => {
+            return axCall().then(result => ({ [index + 1]: result }));
         });
 
-        // Execute all AJAX calls and wait for all promises to resolve
         Promise.all(promises)
-            .then(function (results) {
-                // Combine the results into a single object
-                const context = results.reduce(function (acc, result) {
-                    return Object.assign(acc, result);
-                }, {});
-
-                // Call the callback function with the context as an argument
+            .then(results => {
+                const context = results.reduce((acc, result) => Object.assign(acc, result), {});
                 callback(context);
             })
-            .catch(function (error) {
+            .catch(error => {
                 console.error("Error occurred during AJAX calls:", error);
             });
     }
@@ -44,6 +25,6 @@ function axCall2() {
 }
 
 // Usage of async.getAll
-async.getAll([axCall1(), axCall2()], function (context) {
+async.getAll([axCall1, axCall2], function(context) {
     console.log("Expected context of callback:", context);
-});  
+});
