cd90b9cbb00b81f68a9dedc21d5d6e98e071e86a
lfaustinosITS <132519618+lfaustinosITS@users.noreply.github.com>
2023-09-05 10:14:17 -0600

    Improved code
    The callback is called with an object as argument.

--- JSQ-28-ajaxArray.js
+++ JSQ-28-ajaxArray.js
@@ -1,30 +1,45 @@
 var async = {
     getAll: function (urlArray, callback) {
-        const promises = urlArray.map((axCall, index) => {
-            return axCall().then(result => ({ [index + 1]: result }));
+        let promises = []; // Create an array to store all the promises for AJAX calls
+
+        urlArray.forEach(function (url, index) {// Perform all AJAX calls and store the promises in the array
+            let promise = new Promise(function (resolve, reject) {
+                fetch(url)// Perform the AJAX call
+                    .then(function (response) {
+                        if (response.ok) {
+                            return response.json();
+                        } else {
+                            throw new Error('Error in AJAX call');
+                        }
+                    })
+                    .then(function (data) {
+                        resolve({ [index + 1]: data }); // Resolve the promise with an object containing the result and the corresponding key
+                    })
+                    .catch(function (error) {
+                        reject(error); // Reject the promise if an error occurs
+                    });
+            });
+
+            promises.push(promise);
         });
 
+        // Use Promise.all to wait for all promises to complete
         Promise.all(promises)
-            .then(results => {
-                const context = results.reduce((acc, result) => Object.assign(acc, result), {});
-                callback(context);
+            .then(function (results) {
+                let resultObject = {};// Create an object that contains the results in the expected format
+                results.forEach(function (result) {
+                    Object.assign(resultObject, result);
+                });
+                callback(resultObject);// Call the callback function with the result object as an argument
             })
-            .catch(error => {
-                console.error("Error occurred during AJAX calls:", error);
+            .catch(function (error) {
+                console.error('Error:', error);
             });
-    }
+    },
 };
 
-// Sample AJAX calls
-function axCall1() {
-    return fetch('url1').then(response => response.json());
-}
-
-function axCall2() {
-    return fetch('url2').then(response => response.json());
-}
 
 // Usage of async.getAll
-async.getAll([axCall1, axCall2], function(context) {
+async.getAll([axCall1, axCall2], function (context) {
     console.log("Expected context of callback:", context);
 });
